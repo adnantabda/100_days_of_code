d69ef06117ee099ed08e274ee5c5563d1306c3d2
Tomas Borelli <tomasborelli@protonmail.com>
2022-06-07 22:40:44 -0300

    end messages; lives; quit option; comments; ascii-title

--- challenge/hangman.py
+++ challenge/hangman.py
@@ -5,6 +5,20 @@ Day # 7
 """
 
 import random
+import sys
+
+# Title
+print("""
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                
+"""
+      )
 
 # TODO use a file or api to get random words
 word_list = ["aardvark", "baboon", "camel"]
@@ -17,20 +31,25 @@ display_word = ["_"] * len(chosen_word)
 # Guessing chances
 lives = 6
 
-# TODO ascii-art or something prettier
-
-
-print(" ".join(display_word))
+print("Word:")
+print(f'\t\t{" ".join(display_word)}')
 
 # Repeat till there are no more blanks to display or lose all lives
 while "_" in display_word and lives > 0:
-    guess = input("Guess a letter: ")
+    print("\n(Type 'quit' to exit the game)")
+    guess = input("Guess a letter: ").lower()
+
+    # Quit game
+    if guess == "quit":
+        print("Good bye.")
+        sys.exit()
 
     # Check user not to enter more than 1 LETTER
     while len(guess) != 1 or not guess.isalpha():
         print("I said A letter!")
         guess = input("\nGuess a letter: ").lower()
 
+    # Guessed letter match a letter in word
     if guess in chosen_word:
         print("Correct!")
         # Obtain the index of each letter
@@ -39,6 +58,7 @@ while "_" in display_word and lives > 0:
                 # Change the blank for the letter
                 display_word[index] = letter
 
+    # No match. Loose a life
     else:
         print(f"You loose a life.")
         lives -= 1
@@ -47,15 +67,13 @@ while "_" in display_word and lives > 0:
 
     # Display the word with it's blanks and remaining lives
     print("\n" + " ".join(display_word))
-    print(f"Remaining lives: {lives}")
+    print(f"\nRemaining lives: {lives}")
 
-print("GAME OVER")
+# End game
+print("\nGAME OVER")
 if lives == 0:
     print("YOU'VE LOST")
     print(f"The word was: {chosen_word}")
 else:
     print("YOU WIN!!!")
-
-
-
 
