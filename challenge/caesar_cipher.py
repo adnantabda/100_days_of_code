6f7c35534ee8b497e52261bd9322f620e7931069
Tomas Borelli <tomasborelli@protonmail.com>
2022-06-23 14:39:32 -0300

    merged encrypt/decrypt functions; new functions(repeat, msg, shift_code)

--- challenge/caesar_cipher.py
+++ challenge/caesar_cipher.py
@@ -11,68 +11,114 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             ]
 
 
-def encrypt(original_msg: str, shift_key: int):
-    encrypted_msg = ""
-    for letter in original_msg:
-        if letter not in alphabet:
-            encrypted_msg += letter
+# TODO 2: loop asking the user till inputs "no"
+
+def caesar(order: str, text: str, shift_key: int):
+    """Function to encode/decode a given message."""
+    new_message = ""
+    # To substract the given shift_key and decode the message
+    if order == "decode":
+        shift_key *= -1
+
+    for char in text:
+        # To exclude symbols and numbers from bein encrypted/decrypted
+        if char in alphabet:
+            index = alphabet.index(char)
+            cipher_index = index + shift_key
+            cipher_char = alphabet[cipher_index]
+            new_message += cipher_char
+        # Char not in alphabet
         else:
-            original_index = alphabet.index(letter)
-            encrypted_index = original_index + shift_key
-            encrypted_letter = alphabet[encrypted_index]
-            encrypted_msg += encrypted_letter
+            new_message += char
 
-    print(f"The encrypted message is: {encrypted_msg}")
+    print(f"Your {order}d message is: {new_message}")
 
 
-def decrypt(encrypted_msg: str, shift_key: int):
-    decrypted_msg = ""
-    for letter in encrypted_msg:
-        if letter not in alphabet:
-            decrypted_msg += letter
-        else:
-            encrypted_index = alphabet.index(letter)
-            decrypted_index = encrypted_index - shift_key
-            decrypted_letter = alphabet[decrypted_index]
-            decrypted_msg += decrypted_letter
-
-    print(f"The decoded message is: {decrypted_msg}")
-
+# def encrypt(original_msg: str, shift_key: int):
+#     encrypted_msg = ""
+#     for letter in original_msg:
+#         # To exclude symbols and numbers from being encrypted
+#         if letter not in alphabet:
+#             encrypted_msg += letter
+#         else:
+#             original_index = alphabet.index(letter)
+#             encrypted_index = original_index + shift_key
+#             encrypted_letter = alphabet[encrypted_index]
+#             encrypted_msg += encrypted_letter
+#
+#     print(f"The encrypted message is: {encrypted_msg}")
+#
+#
+# def decrypt(encrypted_msg: str, shift_key: int):
+#     decrypted_msg = ""
+#     for letter in encrypted_msg:
+#         # To exclude symbols and numbers from being decrypted
+#         if letter not in alphabet:
+#             decrypted_msg += letter
+#         else:
+#             encrypted_index = alphabet.index(letter)
+#             decrypted_index = encrypted_index - shift_key
+#             decrypted_letter = alphabet[decrypted_index]
+#             decrypted_msg += decrypted_letter
+#
+#     print(f"The decoded message is: {decrypted_msg}")
+#
 
 # Instruct whether to encrypt or decrypt a message
-print('Type "encode" to encrypt your message or "decode" to decrypt a message')
-instruction = input().lower()
-# Check if the right instruction was given by the user input
 
-while instruction != "encode" and instruction != "decode":
-    print(f'"{instruction}" unknown instruction')
+
+# Instruction
+
+def instruct():
+    """Order given by the user to encrypt or decrypt a message."""
     print('Type "encode" to encrypt your message or "decode" to decrypt a message')
     instruction = input().lower()
+    # Check if the right instruction was given by the user input
+    while instruction != "encode" and instruction != "decode":
+        print(f'"{instruction}" unknown instruction')
+        print('Type "encode" to encrypt your message or "decode" to decrypt a message')
+        instruction = input().lower()
+    return f"{instruction}"
 
 
-# Message to encrypt/decrypt
-print("Type the message")
-message = input().lower()
+def msg():
+    """Message to encrypt/decrypt."""
+    print("Type the message")
+    message = input().lower()
+    return message
 
 
-# Shift to encrypt/decrypt message
-while True:
-    print("Type the shift number")
-    shift = input()
-    # Check if it's a valid shift value
-    try:
-        shift = int(shift)
-        # For indexing purposes the alphabet is repeated. Only makes sense to
-        # shift the msg using the first alphabet of the list
-        if shift < (len(alphabet) / 2):
+def shift_code():
+    """Shift to encrypt/decrypt message - shift_key."""
+    while True:
+        print("Type the shift number")
+        shift = input()
+        # Check if it's a valid shift value
+        try:
+            # For indexing purposes the alphabet is repeated. Only makes sense to
+            # shift the msg using the first alphabet of the list
+            shift = int(shift) % 26
+            if shift < 1:
+                print("Shift out of range")
             break
-        else:
-            print("Shift out of range")
-    except ValueError:
-        print("Only numbers")
+
+        except ValueError:
+            print("Only numbers")
+
+    return shift
 
 
-if instruction == 'encode':
-    encrypt(message, shift)
-else:
-    decrypt(message, shift)
+caesar(order=instruct(), text=msg(), shift_key=shift_code())
+
+# Repeat
+while True:
+    print("\nDo you want to encode/decode a message again?\nType 'yes' or 'no'.")
+    repeat = input().lower()
+    if repeat == "no":
+        break
+    elif repeat == "yes":
+        caesar(order=instruct(), text=msg(), shift_key=shift_code())
+    else:
+        print(f"{repeat} unknown instruction. Type 'yes' or 'no'.")
+
+
