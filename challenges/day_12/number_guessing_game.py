36dcfc7cbad390b0c0e26d2de8d300631403ff8a
Tomas Borelli <tomasborelli@protonmail.com>
2022-07-11 13:09:30 -0300

    number guessing game refactoring and tidier

--- challenges/day_12/number_guessing_game.py
+++ challenges/day_12/number_guessing_game.py
@@ -2,56 +2,67 @@
 Project for Angela Wu's 100 days of code challenges.
 Day # 12
 """
-
 import random
 
-def guessing_game():
+title = """
+╔╗╔┬ ┬┌┬┐┌┐ ┌─┐┬─┐  ╔═╗┬ ┬┌─┐┌─┐┌─┐┬┌┐┌┌─┐  ╔═╗┌─┐┌┬┐┌─┐
+║║║│ ││││├┴┐├┤ ├┬┘  ║ ╦│ │├┤ └─┐└─┐│││││ ┬  ║ ╦├─┤│││├┤ 
+╝╚╝└─┘┴ ┴└─┘└─┘┴└─  ╚═╝└─┘└─┘└─┘└─┘┴┘└┘└─┘  ╚═╝┴ ┴┴ ┴└─┘
+"""
 
+
+def select_level():
+    """Choose what level you want to play."""
+    difficulty = input("Select the difficulty level; enter 'easy' or 'hard': ").lower()
+    while difficulty != "easy" and difficulty != "hard":
+        difficulty = input("Not a valid level. Enter 'EASY' or 'HARD': ").lower()
+    return difficulty
+
+
+def guess_number():
+    """Try to guess the number."""
+    number = input("Make a guess: ")
+    while not number.isdecimal():  # make sure it will be a valid number
+        number = input("That's not a valid number.\nMake a guess: ")
+    number = int(number)
+    return number
+
+
+def guessing_game():
+    """Game where you hace to guess a number between 1 and 100."""
     print("Welcome to the Number Guessing Game, my friend.")
     print("So... I heard you're saying you can guess what other people are thinking of...")
     print("\nI'm thinking of a number between 1 and 100.")
-    print("Let's see what are you made of!\n\n\n")
-    difficulty = input("Select the difficulty level; enter 'easy' or 'hard': ").lower()
+    print("Let's see what are you made of!\n\n")
 
-    while difficulty != "easy" and difficulty != "hard":
-        difficulty = input("Select the difficulty level; enter 'EASY' or 'HARD': ").lower()
-
-    if difficulty == "easy":
+    level = select_level()
+    if level == "easy":
         attempts = 10
-    else:
+    else:  # Hard
         attempts = 5
+    print(f"\nOk, so you chose level {level}. You'll have {attempts} attempts.")
 
-
-    print(f"Ok, so you chose level {difficulty}")
     number = random.randint(1, 100)
-    print(f"Pssssssssttt, hint. The number is {number}")
-    guess = input("Make a guess: ")
+    guess = guess_number()
 
-    while not guess.isdecimal():
-        guess = input("That's not a valid number.\nMake a guess: ")
-
-    while guess != number and attempts > 0:
-        guess = int(guess)
-        attempts -= 1
-
+    while guess != number and attempts > 1:
+        attempts -= 1  # loose a life
         if guess < number:
             print("Too low.")
-        elif guess > number:
+        if guess > number:
             print("Too high.")
-        else:
-            continue
-        print(f"Remaining attempts: {attempts}")
-        guess = input("Make a guess: ")
-        while not guess.isdecimal():
-            guess = input("That's not a valid number.\nMake a guess: ")
-
-    if guess == number and attempts >= 0:
-        print("Congratulations, you've guessed my number.")
-    else:
-        print(f"You've lost!\nI was thinking of number {number}")
+        print(f"\nRemaining attempts: {attempts}")
+        guess = guess_number()
+    if guess == number and attempts >= 0:  # User wins
+        print("\nCongratulations, you've guessed my number.")
+    else:  # User lost
+        print(f"\nYou've lost!\nI was thinking of number {number}")
 
 
+###################################################################
+
+print(title)
 guessing_game()
-
-while input("Do you want to play again? 'y' or 'n': ").lower() == 'y':
+# Repeat
+while input("\n\nDo you want to play again? 'y' or 'n': ").lower() == 'y':
     guessing_game()
