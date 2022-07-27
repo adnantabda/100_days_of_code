03ff6bbf7c0bb18179aa6193dca0b02ff341814b
Tomas Borelli <tomasborelli@protonmail.com>
2022-07-27 18:14:31 -0300

    faking damien hirst refactoring

--- challenges/FakingDamienHirst/main.py
+++ challenges/FakingDamienHirst/main.py
@@ -13,15 +13,15 @@ for color in colors:
     new_color = (r, g, b)
     colors_list.append(new_color)
 
+# Screen config
+screen = Screen()
+screen.title("Faking Damien Hirst")
 colormode(255)  # Documentation (to use rgb colors between 0 and 255)
 
 # turtle initial settings
-artist = Turtle()
+artist = Turtle(shape="classic", visible=False)
 artist.penup()      # Doesn't draw when moves
-artist.pensize(10)
-artist.hideturtle()
 artist.speed("fastest")
-artist.shape("classic")
 
 # Setting starting position
 x_position = -230
@@ -30,13 +30,10 @@ artist.goto(x_position, y_position)
 
 while y_position < 300:                             # 10 rows
     for _ in range(10):                             # 10 columns
-        artist.color(random.choice(colors_list))
-        artist.dot()                                # Draw a dot on screen
+        artist.dot(20, random.choice(colors_list))  # Draw a dot size 20; color random
         artist.forward(50)
     y_position += 50
     artist.goto(x_position, y_position)             # Go up for another row
 
 # Click on screen to end and exit
-screen = Screen()
 screen.exitonclick()
-
